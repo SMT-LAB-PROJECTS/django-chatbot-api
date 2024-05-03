5a492dcf8dddfe1b949ba4affae699209a8204b0
Tarun Upadhyay <tarunsmtgroup@gmail.com>
2024-05-03 20:14:55 +0530

    admin url changes

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -19,7 +19,8 @@ from django.urls import include, path
 from apis import urls as chatbot_url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('chatbot/', include(chatbot_url)),
 ]
