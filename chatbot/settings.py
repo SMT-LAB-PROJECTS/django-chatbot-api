44e028475beac88bf0f6cc4f169ad0879453acd2
Tarun Upadhyay <tarunsmtgroup@gmail.com>
2024-05-03 20:05:32 +0530

    change setting.py

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-hesw21(b@q*mm-h=@0+eac(=oxnxa#a+j8xm+4ta^=$ub4)e-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
